2baae556b4fe07f1a16cea11f2aaa90a7f5b173b
Agus <agus.luu@gmail.com>
2021-05-13 23:05:26 -0300

    Imagenes + header

--- recursos/templates/header.php
+++ recursos/templates/header.php
@@ -1,18 +1,21 @@
 <header>
-    <div>
-
+<nav>
+        <div class="container" style="margin-top:10px;">
             <img style="width: 100px; height: 100px;" src="recursos/imagenes/logo.png">
+
+            <h1 class="navbar-brand" href="#">Pokedex</h1>
             <!--Logo-->
 
-            <h1>Pokedex</h1>
+
             <!--User-->
-            <form class="form-inline" action="paginas/login.php" method="POST">
+            <form class="form-inline" style="float: right;" action="paginas/login.php" method="POST">
                 <label class="mb-2 mr-sm-2 text-white" for="usuario">Usuario:</label>
-                <input class="form-control mb-2 mr-sm-2" type="text" name="usuario" placeholder="usuario" id="usuario">
+                <input class=" form-control mb-2 mr-sm-2" type="text" name="usuario" placeholder="usuario" id="usuario">
                 <label class="mb-2 mr-sm-2 text-white" for="contraseia">Contraseña:</label>
                 <input class="form-control mb-2 mr-sm-2"  type="password" name="contrasenia" placeholder="clave" id="contrasenia">
                 <button type="submit" class="btn btn-primary mb-2">Ingresar</button>
             </form>
+        </div>
 
-    </div>
+</nav>
 </header>
